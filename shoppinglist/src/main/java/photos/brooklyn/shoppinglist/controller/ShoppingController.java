a446bbd4de5fe4732e5d4056e83d0c011aea57c8
brooklynphotos <gzhong@192.168.0.102>
2017-07-08 23:30:35 -0400

    some html and rest content

---
+++ shoppinglist/src/main/java/photos/brooklyn/shoppinglist/controller/ShoppingController.java
@@ -0,0 +1,27 @@
+package photos.brooklyn.shoppinglist.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import photos.brooklyn.shoppinglist.domain.ShoppingItem;
+
+@RestController
+@RequestMapping("/shopping")
+public class ShoppingController {
+	private static final Logger logger = LoggerFactory.getLogger(ShoppingController.class);
+	
+	@RequestMapping(value="/list",method=RequestMethod.GET)
+	public List<ShoppingItem> listShoppingItems(){
+		final List<ShoppingItem> items = new ArrayList<>();
+		ShoppingItem e1 = new ShoppingItem();
+		e1.setId(1);
+		items.add(e1 );
+		return items;
+	}
+}
