d1a1a359bbe7f3bc4257ed4520b3e702f12053f9
gzhong <guoneng.list@gmail.com>
2017-10-17 00:10:34 -0400

    set up basic crud files for shop

--- shoppinglist/src/main/java/photos/brooklyn/shoppinglist/controller/ShoppingController.java
+++ shoppinglist/src/main/java/photos/brooklyn/shoppinglist/controller/ShoppingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import photos.brooklyn.shoppinglist.domain.ShoppingItem;
+import photos.brooklyn.shoppinglist.domain.Item;
 
 @RestController
 @RequestMapping("/shopping")
@@ -17,11 +17,19 @@ public class ShoppingController {
 	private static final Logger logger = LoggerFactory.getLogger(ShoppingController.class);
 	
 	@RequestMapping(value="/list",method=RequestMethod.GET)
-	public List<ShoppingItem> listShoppingItems(){
-		final List<ShoppingItem> items = new ArrayList<>();
-		ShoppingItem e1 = new ShoppingItem();
+	public List<Item> listShoppingItems(){
+		final List<Item> items = new ArrayList<>();
+		Item e1 = new Item();
 		e1.setId(1);
 		items.add(e1 );
 		return items;
+	}
+	@RequestMapping(value="/update",method=RequestMethod.POST)
+	public Item update(Item item){
+		return item;
+	}
+	@RequestMapping(value="/add",method=RequestMethod.PUT)
+	public Item add(Item item){
+		return item;
 	}
 }
