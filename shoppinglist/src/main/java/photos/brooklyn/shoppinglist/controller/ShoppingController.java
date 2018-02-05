410ec458e2c0c2abc6deed9dffb575941e4d1cf8
Brooklyn Photos <guoneng.list@gmail.com>
2018-02-05 16:28:23 -0500

    Merge branch 'master' into dev
    # Conflicts:
    #	shoppinglist/pom.xml
    #	shoppinglist/src/main/java/photos/brooklyn/shoppinglist/controller/ShoppingController.java
    #	shoppinglist/src/main/java/photos/brooklyn/shoppinglist/domain/ShoppingItem.java

--- shoppinglist/src/main/java/photos/brooklyn/shoppinglist/controller/ShoppingController.java
+++ shoppinglist/src/main/java/photos/brooklyn/shoppinglist/controller/ShoppingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import photos.brooklyn.shoppinglist.domain.Item;
 import photos.brooklyn.shoppinglist.domain.ShoppingItem;
 
 @RestController
